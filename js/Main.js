000589ea01fa524fbf69980a74b7814de53657a2
heson <heson525@163.com>
2020-10-02 14:51:03 +0800

    Site updated: 2020-10-02 14:51:01

--- js/Main.js
+++ js/Main.js
@@ -922,4 +922,25 @@ searchFunc(path, 'local-search-input', 'local-search-result');
 			$('#formobile,.formobile').removeClass('d-n').addClass('d-d');
 
 		 } 
-	}); 
+	
+	
+		 
+	
+		}); 
+
+
+		
+			var now = new Date();
+			function createtime() {
+			var grt= new Date('2020/07/06 00:00:00');//此处修改你的建站时间或者网站上线时间
+			now.setTime(now.getTime()+250);
+			days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
+			hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
+			if(String(hnum).length ==1 ){hnum = "0" + hnum;} minutes = (now - grt ) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+			mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
+			seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+			snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
+			document.getElementById("timeDate").innerHTML = "本站已安全运行 "+dnum+" 天 ";
+			document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
+		}
+	 setInterval("createtime()",250);
